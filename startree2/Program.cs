d0c5449cdf00efd49ea255957aca8c4d61f9960e
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 12:19:42 +0900

    별찍기 예제

--- startree2/Program.cs
+++ startree2/Program.cs
@@ -10,6 +10,20 @@ namespace startree2
     {
         static void Main(string[] args)
         {
+            // 밑 줄 15
+            for (int i = 0; i < 8; i++) // 중간이 8
+            {
+                for (int j = 7; j > i; j--)
+                {
+                    Console.Write(" ");
+                } // i = 0
+
+                for (int k = 0; k < (i*2) + 1; k++) // k = 1 처음에 k 가 1이라서 *을 하나만 찍는다.
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
         }
     }
 }
